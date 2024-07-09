d03be4fc213b11d7308ad73b778c9e277ddcf8db
Vikash Khatri <102739330+VikashKhatri95@users.noreply.github.com>
2024-07-09 08:53:42 +0000

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,22 @@
 const socket=io();//Connection request
-console.log("hey");
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude,longitude}=position.coords;
+        socket.emit("send-location",{latitude,longitude});
+    },(error)=>{
+        console.log(error);
+    },
+{
+    enableHighAccuracy:true,
+    timeout:5000,
+    maximumAge:0,//No caching
+}
+); 
+    
+}
+
+const map=L.map("map").setView([0,0],10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution:"Open/streetMap"
+}).addTo(map)
